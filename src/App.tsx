aba73e58c04fba5513815e8a68dbf2c0b97621f9
Orlandini Sebastien <sebastien.orlandini@gmail.com>
2024-01-06 22:06:36 +0100

    conditions

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
 	const [submit, setSubmit] = useState(false);
 	const [alert, setAlert] = useState("");
 	const [hideP1, sethideP1] = useState(true);
-		const [hideP2, sethideP2] = useState(true);
+	const [hideP2, sethideP2] = useState(true);
 
 	const handleNameChange = (event: any) => {
 		const value = event.target.value;
@@ -36,19 +36,29 @@ function App() {
 	};
 	const handleSubmit = (event: any) => {
 		event.preventDefault();
-		if (p1 !== p2) {
+		if (name === "") {
+			setAlert("name is require !");
+			setSubmit(false);
+		} else if (email === "") {
+			setAlert("email is require !");
+			setSubmit(false);
+		} else if (p1.length < 6) {
+			setAlert("password : 7 charachers minimum !");
+			setSubmit(false);
+		} else if (p1 !== p2) {
 			setAlert("password are not the same");
+			setSubmit(false);
 		} else {
 			setAlert("");
 			setSubmit((prev) => !prev);
 		}
 	};
 	const handleHideP1 = () => {
-		sethideP1(prev => !prev);
-	}
-		const handleHideP2 = () => {
-		sethideP2(prev => !prev);
-	}
+		sethideP1((prev) => !prev);
+	};
+	const handleHideP2 = () => {
+		sethideP2((prev) => !prev);
+	};
 	return (
 		<>
 			<Header
@@ -88,39 +98,41 @@ function App() {
 						/>
 						<label>Password</label>
 
-
 						<div className="inputContainer">
 							<input
-								type={hideP1 ? "password" :"text" }    
+								type={hideP1 ? "password" : "text"}
 								placeholder="azerty"
 								name="p1"
 								value={p1}
 								onChange={handleP1Change}
 								className={alert === "" ? "" : "borderRed"}
 							/>
-							<FontAwesomeIcon icon={hideP1 ? "eye" :"eye-slash" }  onClick={handleHideP1} />
+							<FontAwesomeIcon
+								icon={hideP1 ? "eye" : "eye-slash"}
+								onClick={handleHideP1}
+							/>
 						</div>
 
 						<label>Confirm your Password</label>
 						<div className="inputContainer">
 							<input
-								type={hideP2 ? "password" :"text" }    
+								type={hideP2 ? "password" : "text"}
 								placeholder="azerty"
 								name="p2"
 								value={p2}
 								onChange={handleP2Change}
 								className={alert === "" ? "" : "borderRed"}
 							/>
-							<FontAwesomeIcon icon={hideP2 ? "eye" :"eye-slash" } onClick={handleHideP2}/>
+							<FontAwesomeIcon
+								icon={hideP2 ? "eye" : "eye-slash"}
+								onClick={handleHideP2}
+							/>
 						</div>
-
 						<p className="alert">{alert}</p>
-
 						<button>Register</button>
 					</form>
 				)}
 			</section>
-
 			<Footer />
 		</>
 	);
